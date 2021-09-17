bbc99e02eb8d7d295581bdf1ecb509fae4066f05
Eрматова Нургул <87805458+nurgulermatova@users.noreply.github.com>
2021-09-17 13:47:34 +0300

    add file

---
+++ script.js
@@ -0,0 +1,50 @@
+// let userName;
+
+// userName = "Nurgul";
+
+// let userName = "Nurgul";
+
+// let $ = 100;
+// document.write($);
+
+// let userName = 500;
+
+// userName = "500";
+
+
+// document.write(userName + "<br>");
+
+// userName = "John";
+// document.write(userName);
+
+// const pi = 3.14;
+// document.write(pi);
+
+
+
+let userName = prompt("Как вас зовут?");
+let userSurname = prompt("Ваше фамилия?");
+let userAge = prompt("Сколько вам лет?");
+let userYouLive = prompt("Где вы живете?");
+
+
+document.write("<h2>Как вас зовут: " + userName + "</h2>");
+document.write("<h2>Ваше фамилия: " + userSurname+ "</h2>");
+document.write("<h2>Сколько вам лет: " + userAge + "</h2>");
+document.write("<h2>Где вы живете: " + userYouLive+ "</h2>");
+
+
+  let myName = "Нургул";
+  let mail="nurgul.ermatova.kk@gmail.com";
+
+  document.write("Привет!  Меня зовут " + myName + " и мой mail: "+ mail + "<br>");
+
+
+  let x = 1000;
+  let y = 1021;
+
+  let result = x + y;
+
+  document.write(result);
+
+  console.log(result);
